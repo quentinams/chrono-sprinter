ce263c0c6247ce80cdeb0692b37c7448336d12fb
Julien Sery <julien.sery@epitech.eu>
2023-12-02 15:12:19 +0100

    enemy shoot

--- scene/enemy.gd
+++ scene/enemy.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
+var bullet = preload("res://scene/bullet.tscn")
 var speed
 var velocity
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_node("shoot_cooldown").start()
 	pass # Replace with function body.
 
-
+func shoot_player():
+	var new_bullet = bullet.instantiate()
+	var player = get_parent().get_node("Player")
+	new_bullet.global_position = self.global_position
+	new_bullet.velocity = Vector2(1000,0).rotated((player.global_position - new_bullet.position).angle())
+	get_parent().add_child(new_bullet)
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += velocity * delta
@@ -15,3 +22,10 @@ func _process(delta):
 		queue_free()
 	pass
 	
+
+
+func _on_shoot_cooldown_timeout():
+	shoot_player()
+	get_node("shoot_cooldown").stop()
+	get_node("shoot_cooldown").start()
+	pass # Replace with function body.
