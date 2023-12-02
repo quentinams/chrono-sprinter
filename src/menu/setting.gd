d5b107de2c84b6cb2691428a4623eece849bc68b
quentinaumas <quentin.aumas@epitech.eu>
2023-12-02 12:12:26 +0100

    [~] refactor

---
+++ src/menu/setting.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_controle_button_pressed():
+	get_tree().change_scene_to_file("res://scene/control_interface.tscn")
+	
+func _on_sound_button_pressed():
+	print("sound button pressed")
+
+func _on_exit_button_pressed():
+	print("Exit button pressed")
