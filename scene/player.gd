434850cf0fa3e1dc1859488219ec2482e1b87d7b
Julien Sery <julien.sery@epitech.eu>
2023-12-02 12:13:23 +0100

    fix player

--- scene/player.gd
+++ scene/player.gd
@@ -1,8 +1,9 @@
-extends Sprite2D
+extends Node2D
 
-
+var screensize
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	screensize = get_parent().get_node("Camera/Camera2D").get_viewport_rect()
 	pass # Replace with function body.
 
 
@@ -16,6 +17,6 @@ func _process(delta):
 		self.position.x -= 10
 	if Input.is_action_pressed("player_right"):
 		self.position.x += 10
-	position.x = clamp(position.x, $Camera.limit_left, $Camera.limit_right)
-	position.y = clamp(position.y, $Camera.limit_top, $Camera.limit_bottom)
+	position.x = clamp(position.x, screensize.position.x, screensize.end.x)
+	position.y = clamp(position.y, screensize.position.y, screensize.end.y)
 	pass
