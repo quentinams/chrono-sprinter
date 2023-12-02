c772fafa3b8030fcb04c0e20a3a775dcc40447a4
quentinaumas <quentin.aumas@epitech.eu>
2023-12-02 17:08:56 +0100

    [+] setting

--- src/menu/setting.gd
+++ src/menu/setting.gd
@@ -1,10 +1,16 @@
 extends Control
 
+@export
+var bus_name: String
+
+var bus_index: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AudioStreamPlayer.play()
+	bus_index = AudioServer.get_bus_index("Master")
 	$VBoxContainer/ControleButton.grab_focus()
-
+	$VBoxContainer/volume_slider.value = 1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
@@ -18,3 +24,8 @@ func _on_sound_button_pressed():
 func _on_exit_button_pressed():
 	get_tree().change_scene_to_file("res://scene/menu/main_menu.tscn")
 	print("Exit button pressed")
+
+
+func _on_volume_slider_value_changed(value):
+	print(value)
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
