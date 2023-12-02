c1b44afeb22d95a3b6ebbff03dbc93409e22a854
Pierre Lamey <pierre.lamey@epitech.eu>
2023-12-02 11:50:58 +0100

    f

--- scene/player.gd
+++ scene/player.gd
@@ -8,12 +8,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("up"):
+	if Input.is_action_pressed("player_up"):
 		self.position.y -= 10
-	if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("player_down"):
 		self.position.y += 10
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("player_left"):
 		self.position.x -= 10
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("player_right"):
 		self.position.x += 10
 	pass
