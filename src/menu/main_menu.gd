4e5b3b5228ac4a478074a89964ee18fd36a0ba2c
Alexyan Comino <alexyan.comino@epitech.eu>
2023-12-02 14:06:40 +0100

    refactor(): organizing folders.

--- src/menu/main_menu.gd
+++ src/menu/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/StartButton.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,11 +12,11 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scene/menu/main_menu.tscn")
 
 
 func _on_settings_button_pressed():
-	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scene/menu/setting.tscn")
 
 
 func _on_exit_button_pressed():
