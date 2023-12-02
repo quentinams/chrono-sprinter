18df965d9989f86b3f702389c4f27d85db539238
Pierre Lamey <pierre.lamey@epitech.eu>
2023-12-02 15:26:40 +0100

    qlkhfklqjfq

--- scene/player.gd
+++ scene/player.gd
@@ -4,6 +4,7 @@ var speed = 800
 var player_size = Vector2 (128, 128)
 var player_scale = Vector2 (0.5, 0.5)
 var player_start_pos = Vector2 (50, 500)
+var is_moving = false
 @onready var screen_size = get_parent().get_node("Camera/Camera2D").get_viewport_rect()
 
 # Called when the node enters the scene tree for the first time.
@@ -21,12 +22,16 @@ func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("player_up"):
 		velocity.y -= 1
+		is_moving = true
 	if Input.is_action_pressed("player_down"):
 		velocity.y += 1
+		is_moving = true
 	if Input.is_action_pressed("player_left"):
 		velocity.x -= 1
+		is_moving = true
 	if Input.is_action_pressed("player_right"):
 		velocity.x += 1
+		is_moving = true
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	position += velocity * delta
