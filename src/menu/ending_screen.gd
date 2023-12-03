a18d0f8d914f824dfc89584b2b9c74f25c7e501c
Alexyan Comino <alexyan.comino@epitech.eu>
2023-12-03 12:11:23 +0100

    feat(): added ending screen.

---
+++ src/menu/ending_screen.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+var scroll_x = 0
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Scroll background
+	scroll_x -= 200 * delta
+	$ParallaxBackground.scroll_offset.x = scroll_x
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://scene/main.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
